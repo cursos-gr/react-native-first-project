446ab1db08dac04998140844b7a695f22d1bb154
Guilherme Ribeiro (GR) <its.gr6603@gmail.com>
2023-10-27 08:19:16 -0300

    Add files via upload

---
+++ src/mocks/carrinho.js
@@ -0,0 +1,16 @@
+import logo from '../../assets/logo.png';
+
+const carrinho = {
+  topo: {
+    titulo: "Detalhe da cesta",
+  },
+  detalhes: {
+    nome: "Carrinho de Suplementos",
+    logoLoja: logo,
+    nomeFornecedor: "GR's ",
+    descricao: "",
+    preco: "R$ 40,00",
+  }
+}
+
+export default carrinho;
